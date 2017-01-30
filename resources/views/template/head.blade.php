14ee9bf0b5f3fd84d7aa242fe60d346e8fc640f5
Jorge mario <jmla196@gmail.com>
2017-01-30 09:33:21 -0500

    Update

--- resources/views/template/head.blade.php
+++ resources/views/template/head.blade.php
@@ -1,4 +1,4 @@
-<title>Pure Bulk</title>
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="author" content="Ziuxlab">
@@ -15,7 +15,7 @@
 <link rel="icon" type="image/png" href="favicons/favicon-32x32.png" sizes="32x32">
 <link rel="icon" type="image/png" href="favicons/favicon-16x16.png" sizes="16x16">
 <link rel="manifest" href="favicons/manifest.json">
-<link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="mask-icon" href="favicons/safari-pinned-tab.svg">
 <meta name="theme-color" content="#ffffff">
 
 <link rel="manifest" href="/manifest.json">
